667a04710011544d96ce53818e37807d74f2f390
Eaoumoon Haque <22-47526-2@student.aiub.edu>
2026-04-15 20:59:16 +0600

    Friend card created and friends list shown

--- src/Component/Homepage/FriendsList.jsx
+++ src/Component/Homepage/FriendsList.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import useFriends from '../../hooks/useFriends';
+import { RingLoader } from 'react-spinners';
+import FriendCard from '../../UI/FriendCard';
+
 
 const FriendsList = () => {
     const {friends,loading} = useFriends();
@@ -8,10 +11,18 @@ const FriendsList = () => {
         <div className='container mx-auto '>
             <p className='mb-4 text-2xl font-semibold'>Your Friends</p>
             {
-                friends.map(()=>{
-                    loading ? "" : ""
-                })
+                    loading ? ( 
+                    <div className='flex items-center justify-center p-10'>
+                        <RingLoader />
+                    </div> 
+                    ) : 
+                    ( <div className='grid gap-6 grid-cols-1 mb-20 md:grid-cols-2 lg:grid-cols-3'>
+                        {
+                            friends.map((friend,idx)=> <FriendCard friend={friend} key={idx}></FriendCard>)
+                        }
+                    </div> )
             }
+            
         </div>
     );
 };
--- src/UI/FriendCard.jsx
+++ src/UI/FriendCard.jsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import React from "react";
 
-const FriendCard = () => {
-    return (
-        <div>
-            friend
+const FriendCard = ({ friend }) => {
+  const { tags } = friend;
+  return (
+    <div className=" bg-base-100 w-96 shadow-xl">
+      <figure className="px-10 pt-10">
+        <img src={friend.picture} alt={friend.name} className="rounded-xl mx-auto" />
+      </figure>
+      <div className="card-body items-center text-center">
+        <h2 className="card-title">{friend.name}</h2>
+        <p className="text-[#64748bFF]">{friend.days_since_contact}d ago</p>
+        <div className="flex justify-center items-center space-x-1">
+          {tags.map((tag, idx) => {
+            return (
+              <button key={idx} className="btn bg-[#cbfadbFF] rounded-3xl">
+                {tag}
+              </button>
+            );
+          })}
         </div>
-    );
+        <div className="card-actions">
+          <button className="btn btn-primary rounded-3xl">{friend.status}</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default FriendCard;
