558e6e06b0e9583d9a892e75acac690ca7474d1a
Eaoumoon Haque <22-47526-2@student.aiub.edu>
2026-04-15 21:47:24 +0600

    stats updates dynamically

--- src/Component/Homepage/CountStat.jsx
+++ src/Component/Homepage/CountStat.jsx
@@ -1,19 +1,41 @@
 import React from "react";
+import useFriends from "../../hooks/useFriends";
+import { RingLoader } from "react-spinners";
 
 const CountStat = () => {
+  const { friends, loading } = useFriends();
+  if (loading)
+    return (
+      <div className="flex items-center justify-center p-10">
+        <RingLoader />
+      </div>
+    );
+
+  const onTrackCount = friends.filter(
+    (friend) => friend.status === "On-Track").length;
+
+  const needAttentionCount = friends.filter(
+    (friend) => friend.status === "Overdue").length;
+    
   return (
     <div className="container mx-auto">
       <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 text-center mb-10">
         <div className="shadow-lg p-8 ">
-          <p className="text-3xl font-semibold text-[#244d3fFF]">10</p>
+          <p className="text-3xl font-semibold text-[#244d3fFF]">
+            {friends.length}
+          </p>
           <p className="text-[#64748bFF] text-[18px]">Total Friends</p>
         </div>
         <div className="shadow-lg p-8">
-          <p className="text-3xl font-semibold text-[#244d3fFF]">10</p>
+          <p className="text-3xl font-semibold text-[#244d3fFF]">
+            {onTrackCount}
+          </p>
           <p className="text-[#64748bFF] text-[18px]">On Track</p>
         </div>
         <div className="shadow-lg p-8">
-          <p className="text-3xl font-semibold text-[#244d3fFF]">10</p>
+          <p className="text-3xl font-semibold text-[#244d3fFF]">
+            {needAttentionCount}
+          </p>
           <p className="text-[#64748bFF] text-[18px]">Need Attention</p>
         </div>
         <div className="shadow-lg p-8">
