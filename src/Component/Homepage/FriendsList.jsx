f7cade72ce9a043a4bf7af01401913035d4d2da1
Eaoumoon Haque <22-47526-2@student.aiub.edu>
2026-04-15 20:11:54 +0600

    useFriends hook created

--- src/Component/Homepage/FriendsList.jsx
+++ src/Component/Homepage/FriendsList.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
+import useFriends from '../../hooks/useFriends';
 
 const FriendsList = () => {
+    const {friends,loading} = useFriends();
+    console.log(friends,loading, "friends");
+    
     return (
         <div>
             friends
